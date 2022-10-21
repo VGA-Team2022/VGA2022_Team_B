bc0e60085c502cdc867236be86051fdc708cdcae
MizutamariShutaro <shutaro0415@outlook.jp>
2022-10-21 19:36:33 +0900

    [add] プレイヤーの上下移動作成

--- Assets/Tamari/Script/Player.cs
+++ Assets/Tamari/Script/Player.cs
@@ -3,55 +3,84 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-	[SerializeField, Tooltip("プレイヤーの移動速度")] float _playerSpeed = 2;
+	//[SerializeField, Tooltip("プレイヤーの移動速度")] float _playerSpeed = 2;
 	[SerializeField, Tooltip("ジャイロの速度")] float _playerGyroSpeed;
-	[SerializeField, Tooltip("ジャイロを受け付ける感覚")] float _gyroTime;
+	[SerializeField, Tooltip("ジャイロを受け付ける間隔")] float _gyroTime;
 
 	[Space(10)]
 
-	[SerializeField, Tooltip("上のレーン")] float _upLane;
-	[SerializeField, Tooltip("真ん中のレーン")] Vector2 _centerLane;
-	[SerializeField, Tooltip("下のレーン")] float _downLane;
+	[SerializeField, Tooltip("レーンの位置")] Transform[] _raneNum;
+	[Tooltip("プレイヤーの現在地")] private int _nowPos;
+
+	//プレイヤーの現在地をプロパティ化
+	public int NowPos
+    {
+		get { return _nowPos; }
+    }
 
 	Rigidbody _rb;
-	
 	float time;
 
 	void Start()
 	{
+		_nowPos = 1;
+		gameObject.transform.position = _raneNum[_nowPos].position;
 		_rb = GetComponent<Rigidbody>();
 	}
 
 	void Update()
 	{
 		Move();
-		//GetGyro();
 		Up();
 		Down();
+
+		//GetGyro();
 	}
 
 	/// <summary>
 	/// プレイヤーの移動
 	/// </summary>
-    private void Move()
+	private void Move()
 	{ 
-		//自動で右に進む
-		_rb.velocity = new Vector2(_playerSpeed, _rb.velocity.y);
+		//自動で右に進む?
+		//_rb.velocity = new Vector2(_playerSpeed, _rb.velocity.y);
+		//レーンを更新
+		gameObject.transform.position = _raneNum[_nowPos].position;
 	}
 
+	/// <summary>
+	/// 上のレーンに移動
+	/// </summary>
 	private void Up()
     {
 		if (Input.GetKeyDown(KeyCode.W))
 		{
-
+			if(_nowPos >= _raneNum.Length - 1)
+            {
+				return;
+            }
+			else if(_nowPos < _raneNum.Length)
+            {
+				_nowPos++;
+            }
 		}
     }
+	/// <summary>
+	/// 下のレーンに移動
+	/// </summary>
     private void Down()
     {
         if(Input.GetKeyDown(KeyCode.S))
         {
-
-        }
+			if (_nowPos == 0)
+			{
+				return;
+			}
+			else if (_nowPos > 0)
+			{
+				_nowPos--;
+			}
+		}
     }
     /// <summary>
     /// とりあえずのジャイロ操作
