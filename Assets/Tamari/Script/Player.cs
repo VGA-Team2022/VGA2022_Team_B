b9090fe4c49f0ce9ba8d491c22f66f684a06fa6c
MizutamariShutaro <shutaro0415@outlook.jp>
2022-10-21 16:36:04 +0900

    [fix] Playerスクリプト修正

--- Assets/Tamari/Script/Player.cs
+++ Assets/Tamari/Script/Player.cs
@@ -3,10 +3,19 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
+	[SerializeField, Tooltip("プレイヤーの移動速度")] float _playerSpeed = 2;
+	[SerializeField, Tooltip("ジャイロの速度")] float _playerGyroSpeed;
+	[SerializeField, Tooltip("ジャイロを受け付ける感覚")] float _gyroTime;
+
+	[Space(10)]
+
+	[SerializeField, Tooltip("上のレーン")] float _upLane;
+	[SerializeField, Tooltip("真ん中のレーン")] Vector2 _centerLane;
+	[SerializeField, Tooltip("下のレーン")] float _downLane;
+
 	Rigidbody _rb;
+	
 	float time;
-	[SerializeField] float _playerSpeed = 2;
-	[SerializeField] float _playerGyroSpeed;
 
 	void Start()
 	{
@@ -16,46 +25,48 @@ public class Player : MonoBehaviour
 	void Update()
 	{
 		Move();
-		GetGyro();
+		//GetGyro();
+		Up();
+		Down();
 	}
 
 	/// <summary>
-	/// 自動で進む
+	/// プレイヤーの移動
 	/// </summary>
     private void Move()
-    {
+	{ 
+		//自動で右に進む
 		_rb.velocity = new Vector2(_playerSpeed, _rb.velocity.y);
 	}
 
-	/// <summary>
-	/// とりあえずのジャイロ操作(1案目)
-	/// </summary>
-	private void GetGyro()
+	private void Up()
+    {
+		if (Input.GetKeyDown(KeyCode.W))
+		{
+
+		}
+    }
+    private void Down()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+
+        }
+    }
+    /// <summary>
+    /// とりあえずのジャイロ操作
+    /// </summary>
+    private void GetGyro()
     {
 		time += Time.deltaTime;
-		if (time % 15 == 0)
+		if (time % _gyroTime == 0)
 		{
 			Vector3 acceleration = Input.acceleration;
 			acceleration.x *= 20;
-			acceleration.y *= 40;
+			acceleration.y *= 0;
 			acceleration.z = 0;
 			Physics2D.gravity = acceleration;
 		}
 	}
-
-	/// <summary>
-	/// ジャイロ操作(2案目)
-	/// </summary>
-	private void GetGyro2()
-    {
-		var dire = Vector3.zero;
-
-		dire.z = Input.acceleration.x * -1;
-		dire.x = Input.acceleration.y;
-
-		dire *= Time.deltaTime;
-		transform.Rotate(dire * _playerGyroSpeed);
-	}
-
 
 }
