e704c5cf4351808ec1a8999dfb1ed6caec956f36
ueno-000 <sarasa4869@zoho.com>
2022-10-27 15:32:10 +0900

    [Update] 止まる犬と走り抜ける犬の挙動追加

--- Assets/Dev＿Prototype/Enemy_Dog.cs
+++ Assets/Dev＿Prototype/Enemy_Dog.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum DogType
+{
+    Outrun,//走り抜ける
+    Stop//突然止まる
+}
+
 public class Enemy_Dog : MonoBehaviour
 {
-    /// <summary>
-    /// エネミーのスピード
-    /// </summary>
-    [Tooltip("エネミーにアタッチする"), SerializeField] private float _speed = 2.0f;
+    [Tooltip("犬の種類"), SerializeField] private DogType _dogType = DogType.Outrun;
+
+    /// <summary>エネミーのスピード</summary>
+    private float _speed = 2.0f;
     public float Speed
         { get { return _speed; } set { _speed = value; } }
 
     private StageMove _stageMove;
+
+    /// <summary>止まる犬の止まっている時間</summary>
+    private int _stopTime = 2;
 
     /// <summary>エネミーの軸移動方向</summary>
     private float _startPosX = 0;
@@ -21,7 +30,6 @@ public class Enemy_Dog : MonoBehaviour
         _stageMove = GameObject.Find("StageManager_Yasiki").GetComponent<StageMove>();
         Speed = _stageMove.MoveSpeed;
         _startPosX = transform.position.x;
-        //GetComponent<Transform>().position = this.gameObject.transform.position;
     }
 
     /// <summary>
@@ -29,29 +37,70 @@ public class Enemy_Dog : MonoBehaviour
     /// </summary>
     void FixedUpdate()
     {
-        //スタート位置によって進行方向を変える
-        if (_startPosX >= 0)
+        switch (_dogType)
         {
-            //x += 1;//背景スピード*方向
-           this.gameObject.transform.position -= new Vector3(Time.deltaTime * Speed, 0);
+            case DogType.Outrun:
+
+                    //スタート位置によって進行方向を変える
+                    if (_startPosX >= 0)
+                    {
+                        this.gameObject.transform.position -= new Vector3(Time.deltaTime * Speed, 0);
+                    }
+                    else
+                    {
+                        this.gameObject.transform.position += new Vector3(Time.deltaTime * Speed, 0);
+                    }
+
+                break;
+
+            case DogType.Stop:
+
+                //スタート位置によって進行方向を変える
+                if (_startPosX >= 0)//Playerの進行方向からくる挙動
+                {
+                    this.gameObject.transform.position -= new Vector3(Time.deltaTime * Speed, 0);
+
+                    //ｘ軸の-1～1Ｍ地点に入ったら止まる(Playerの場所)
+                    if (transform.position.x >= -1 && transform.position.x <= 1)
+                    {
+                        StartCoroutine(StopDog(_stopTime));
+                    }
+                    
+                }
+                else//Playerの後ろ方向からくる挙動
+                {
+                    this.gameObject.transform.position += new Vector3(Time.deltaTime * Speed, 0);
+
+                    //ｘ軸の-1～1Ｍ地点に入ったら止まる(Playerの場所)
+                    if (transform.position.x >= -1 && transform.position.x <= 1)
+                    {
+                        StartCoroutine(StopDog(_stopTime));
+                    }
+                }
+
+
+                break;
         }
-        else 
-        {
-            this.gameObject.transform.position += new Vector3(Time.deltaTime * Speed, 0);
-        }
 
 
-        //if (x <= 30 || x >= -30)
-        //{
-        //    Destroy(this.gameObject);
-        //}
-
+        //画面外に行ったら消える
         if (transform.position.x <= -30 || transform.position.x >= 30)
         {
             Destroy(this.gameObject);
         }
     }
 
+
+    //止まる犬
+    private IEnumerator StopDog(float time)
+    {
+        Speed = 0;
+        yield return new WaitForSeconds(time);
+        Speed = _stageMove.MoveSpeed;
+    }
+
+
+    //あたり判定
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
