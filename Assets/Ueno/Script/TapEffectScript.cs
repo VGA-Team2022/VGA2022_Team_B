5acc7945ea31f17ccd4c9478a872421058163823
ueno-000 <sarasa4869@zoho.com>
2022-11-24 23:57:57 +0900

    [Add] 画面タップ時にparticleeffectを表示させる

---
+++ Assets/Ueno/Script/TapEffectScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 画面をタップした時にタップエフェクトを出したい
+/// </summary>
+public class TapEffectScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //particleを表示する
+        }
+    }
+}
