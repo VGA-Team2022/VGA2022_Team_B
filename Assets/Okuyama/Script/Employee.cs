d783da75b3edc1af5e44eb60428b79c4e4a36888
奥山 <83827729+Nana-77-o@users.noreply.github.com>
2022-12-09 20:24:45 +0900

    [Update]ケーキを追加する使用人を一回だけ生成するように変更。まだ追加は出来てない

--- Assets/Okuyama/Script/Employee.cs
+++ Assets/Okuyama/Script/Employee.cs
@@ -8,20 +8,32 @@ public class Employee : MonoBehaviour
 {
     [SerializeField, Tooltip("ケーキ")] 
     GameObject _cake = null;
-    [SerializeField, Tooltip("ケーキ出現場所")] 
+    [Tooltip("ケーキ出現場所")] 
     GameObject _cakePos = null;
+
     StageMove _stageMove;
+    bool _cakeInstance = false;
+    GameObject _obonObj;
 
     void Start()
     {
         _stageMove = GameObject.Find("StageManager").GetComponent<StageMove>();
+        var gimmickManaget = GameObject.Find("GimmickManager").GetComponent<Gimmickmanager>();
+        _cakePos = gimmickManaget.gameObject.transform.GetChild(1).gameObject;
+        _obonObj = GameObject.Find("ObonPos");
     }
     private void FixedUpdate()
     {
         gameObject.transform.position -= new Vector3(Time.deltaTime * _stageMove.MoveSpeed, 0);
-        if(this.gameObject.transform.position.x < 0)
+        if(this.gameObject.transform.position.x <= 0 && _cakeInstance == false)
         {
-            Instantiate(_cake, _cakePos.transform);
+            var cake = Instantiate(_cake, _cakePos.transform);
+            cake.transform.parent = _obonObj.transform;
+            _cakeInstance = true;
+        }
+        if(gameObject.transform.position.x <= -47)
+        {
+            gameObject.SetActive(false);
         }
     }
 }
--- Assets/Okuyama/Script/Employee_cake.cs
+++ Assets/Okuyama/Script/Employee_cake.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 /// <summary>
 /// ケーキを積む使用人用ケーキ
 /// </summary>
 public class Employee_cake : MonoBehaviour
 {
-    SpringJoint _joint = null;
+    [SerializeField]UnityEvent _event;
+    Sweets _sweets;
+    GameObject _obonPos;
+    Obon _obon;
 
-    private void OnCollisionEnter(Collision collision)
+    private void Start()
     {
-        if(collision.gameObject.CompareTag("Cake"))
-        {
-            _joint.connectedBody = collision.rigidbody;
-        }
+        _sweets = GetComponent<Sweets>();
+        _obonPos = GameObject.Find("ObonPos");
+        _sweets._prevObj = _obonPos;
+        _obon = _obonPos.GetComponent<Obon>();
+        _obon.SweetsAdd(gameObject);//ここがうまく行ってない
+        StartCoroutine(aa());
+        
+    }
+
+    IEnumerator aa()//確認用
+    {
+        yield return new WaitForSeconds(1);
+        _event.Invoke();
     }
 }
