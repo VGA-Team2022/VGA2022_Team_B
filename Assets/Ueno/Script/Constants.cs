4754284e819e1518d3ed0772f0a5005257733807
ueno-000 <sarasa4869@zoho.com>
2022-12-15 16:03:18 +0900

    [fix] 変えていたScene名をもとに戻した

--- Assets/Ueno/Script/Constants.cs
+++ Assets/Ueno/Script/Constants.cs
@@ -6,7 +6,7 @@ namespace Common
     public static class Define
     {
         public const string SCENENAME_TITLE = "TitleScene";
-        public const string SCENENAME_HOME = "Tamari_GameScene_É¿";
+        public const string SCENENAME_HOME = "GameScene_É¿";
         public const string SCENENAME_MASTERGAME = "GameScene_É¿";
         public const string SCENENAME_RESULT = "ResultScene";
         public const float GAME_TIME = 90;
