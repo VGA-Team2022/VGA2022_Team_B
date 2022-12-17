ca7d72e0145287976cb9a5c150789f65e8fa9677
wadashin <Shin.86.miso@outlook.com>
2022-12-17 15:31:47 +0900

    犬の当たり判定を作り直した

--- Assets/Wada/WadaFolder/Sweets.cs
+++ Assets/Wada/WadaFolder/Sweets.cs
@@ -89,7 +89,10 @@ public class Sweets : MonoBehaviour
     {
         _prevObj = null;
         this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;//Rigidbodyのロックを解除
+        _anim.enabled = false;
         this.transform.eulerAngles = new Vector3(0, 0,Random.Range(-30,30));//お菓子の向きをランダムに変える
+        //transform.rotation = Quaternion.Euler(0, 0, 90);
+        //this.transform.eulerAngles = new Vector3(90, 90, 90);//お菓子の向きをランダムに変える
         _rb.AddForce(new Vector3(Random.Range(-1, 1), 1,0) * power);//AddForceせんと崩れないからAddForce。演出にも使えソう;
     }
 
