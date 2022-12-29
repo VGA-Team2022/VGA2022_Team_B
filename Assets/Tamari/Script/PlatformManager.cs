bbca4466543680ed2eef826c00a82e2733f3dfd4
MizutamariShutaro <shutaro0415@outlook.jp>
2022-12-29 15:06:05 +0900

    [update] Debug

--- Assets/Tamari/Script/PlatformManager.cs
+++ Assets/Tamari/Script/PlatformManager.cs
@@ -12,14 +12,17 @@ public class PlatformManager : MonoBehaviour
 #if UNITY_EDITOR
         _LRButton.SetActive(true);
         _Gyro.SetActive(true);
+        Debug.Log("Unity_Editor");
 
 #elif UNITY_ANDROID
         _LRButton.SetActive(true);
         _Gyro.SetActive(true);
+        Debug.Log("Unity_Android");
 
 #elif UNITY_STANDALONE_WIN
         _LRButton.SetActive(true);
         _Gyro.SetActive(false);
+        Debug.Log("Unity_Windows");
 
 #endif
 
