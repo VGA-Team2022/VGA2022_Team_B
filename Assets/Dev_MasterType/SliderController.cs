d7f210c78e0e176d64e1a48e04a31ce3f9872817
ueno-000 <sarasa4869@zoho.com>
2023-01-06 06:05:30 +0900

    [Add] プレイヤーの現在値を示すSliderの追加

---
+++ Assets/Dev_MasterType/SliderController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour
+{
+
+    private Slider _slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slider = GetComponent<Slider>();
+        _slider.maxValue = GameManager.GameTimeClearLength;
+        _slider.value = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _slider.value =  GameManager.GameTimeClearLength - GameManager.CurrentTime;
+    }
+}
