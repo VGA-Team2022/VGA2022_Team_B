c0670fb8f6ee9c8363181a28f76a25a160540784
ueno-000 <sarasa4869@zoho.com>
2023-01-06 19:11:33 +0900

    [Update] Clear時にDoor出現のAnim作成

--- Assets/Ueno/Script/DoorTouchClear.cs
+++ Assets/Ueno/Script/DoorTouchClear.cs
@@ -2,7 +2,21 @@ using UnityEngine;
 
 public class DoorTouchClear : MonoBehaviour
 {
-    
+
+    private Animator _anim;
+
+    private void Start()
+    {
+        _anim= GetComponent<Animator>();    
+    }
+
+    private void FixedUpdate()
+    {
+        if (GameManager.isAppearDoorObj)
+        {
+            _anim.SetBool("isClear",true);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -10,7 +24,7 @@ public class DoorTouchClear : MonoBehaviour
         {
             Debug.Log(11111111111);
             GameManager.isGameClear= true;
-
+            Debug.Log($"aaaaaaaaaaaaaaaaaaaa:{GameManager.isGameClear}");
         }
         
     }
