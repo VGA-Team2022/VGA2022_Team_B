fac8e9609869c63d3bc52fd1211fb02bf0352f8d
奥山 <83827729+Nana-77-o@users.noreply.github.com>
2023-01-08 16:03:57 +0900

    「Update」ほかのシーンにGimmickmanagerを対応できるように改良しました。

--- Assets/Dev＿Prototype/Gimmick/Gimmickmanager.cs
+++ Assets/Dev＿Prototype/Gimmick/Gimmickmanager.cs
@@ -1,18 +1,28 @@
 using Common;
+using System;
 using UnityEngine;
+
+[Serializable]
+class GimmickPrefabs
+{
+    [Tooltip("ギミックPrefab"),SerializeField] public GameObject[] _gimmickPrefabs;
+}
 
 /// <summary>
 /// ギミック生成Manager
 /// </summary>
 public class Gimmickmanager : MonoBehaviour
 { 
-    [Header("一番下に使用人を入れる")]
-    [Tooltip("ギミックPrefab"),SerializeField] private GameObject[] _gimmickPrefabs;
+    //[Header("一番下に使用人を入れる")]
+    
+    [Header("Scecn番号と同じElement番号にSceneに合ったPrefabを入れる\n屋敷(昼)が0番ならElement番号0番に屋敷(昼)に使うギミックを入れる\nケーキを追加する使用人は常に一番下に入れる")]
+    [SerializeField] private GimmickPrefabs[] _sceneGimmick;
     [Tooltip("出現するレーン"), SerializeField] private Transform _appearLane;
     [Tooltip("出現する時間"), SerializeField] private float[] _appearTime;
     [Tooltip("StageMove"), SerializeField] private StageMove _stageMove;
     [Tooltip("おぼんObj"), SerializeField] private GameObject _obonObj;
 
+    private GameObject[] _gimmickPrefabs;
     private int _appearGimmickNum = 0;
     private bool isAppearGimmick = false;
     private bool employee = false;
@@ -22,18 +32,7 @@ public class Gimmickmanager : MonoBehaviour
     private void Start()
     {
          _nowStarg = GameManager.GameStageNum;
-        switch (_nowStarg)//シーン番号
-        {
-            case 0:
-                Debug.Log("0");
-                break;
-            case 1:
-                Debug.Log("1");
-                break;
-            case 2:
-                Debug.Log("2");
-                break;
-        }
+        _gimmickPrefabs = _sceneGimmick[_nowStarg]._gimmickPrefabs;
     }
     private void Update()
     {
@@ -49,7 +48,7 @@ public class Gimmickmanager : MonoBehaviour
         if (_appearGimmickNum == _appearTime.Length) { return; }
         if (GameManager.CurrentTime <= _appearTime[_appearGimmickNum] && !isAppearGimmick)
         {
-            var index = Random.Range(0, _gimmickPrefabs.Length -1);
+            var index = UnityEngine.Random.Range(0, _gimmickPrefabs.Length -1);
             GenerateGimmick(index);
             _appearGimmickNum++;
             isAppearGimmick = true;
@@ -60,7 +59,7 @@ public class Gimmickmanager : MonoBehaviour
     {
         var obj = Instantiate(_gimmickPrefabs[gimmickPrefabNum], _appearLane);
         Debug.Log($"{_gimmickPrefabs[gimmickPrefabNum]}が出現した,現在のゲーム内時間{GameManager.CurrentTime}");
-        if (gimmickPrefabNum == TARBANGARL_INDEX_ONE)
+        if (gimmickPrefabNum == TARBANGARL_INDEX_ONE && _nowStarg == 0)//_nowStarg == 0は屋敷(昼)に合った番号にする
         {
             obj.GetComponent<TarbanGarl>().StageMove = _stageMove;
         }
