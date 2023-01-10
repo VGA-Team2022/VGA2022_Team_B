47bf8ab40c9397313603f6766d09779667420515
ueno-000 <sarasa4869@zoho.com>
2023-01-11 01:20:13 +0900

    [Update] UVスクロールを用いて背景を動かす方法に切り替えた

--- Assets/Ueno/Script/StageTypeChange.cs
+++ Assets/Ueno/Script/StageTypeChange.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 public enum GameStageBackGroundType
 {
-    yashiki,
+    yashiki_DayLight,
+    yashik_Night,
     umibe,
-    niwa
 }
 public class StageTypeChange : MonoBehaviour
 {
-    [SerializeField] private GameStageBackGroundType _stageType = GameStageBackGroundType.yashiki;
+    [SerializeField] private GameStageBackGroundType _stageType = GameStageBackGroundType.yashiki_DayLight;
     [Header("アタッチするもの")]
-    [SerializeField] private GameObject[] _wall;
-    [SerializeField] private Sprite[] _backgroundSprites;
+    [SerializeField] private Material[] _targetMaterial;
 
 
     private void OnValidate()
@@ -24,23 +23,11 @@ public class StageTypeChange : MonoBehaviour
     {
         switch (_stageType)
         {
-            case GameStageBackGroundType.yashiki:
-                foreach (var i in _wall)
-                {
-                    i.GetComponent<SpriteRenderer>().sprite = _backgroundSprites[0];
-                }
+            case GameStageBackGroundType.yashiki_DayLight:
+                GetComponent<MeshRenderer>().material = _targetMaterial[0];
                 break;
-            case GameStageBackGroundType.umibe:
-                foreach (var i in _wall)
-                {
-                    i.GetComponent<SpriteRenderer>().sprite = _backgroundSprites[1];
-                }
-                break;
-            case GameStageBackGroundType.niwa:
-                foreach (var i in _wall)
-                {
-                    i.GetComponent<SpriteRenderer>().sprite = _backgroundSprites[2];
-                }
+            case GameStageBackGroundType.yashik_Night:
+                GetComponent<MeshRenderer>().material = _targetMaterial[1];
                 break;
         }
     }
