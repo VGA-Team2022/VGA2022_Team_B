2e4bb23bbbb4e3fd432559cb7bfead1ee58c8184
ueno-000 <sarasa4869@zoho.com>
2023-01-13 15:12:26 +0900

    [fix] Fadein処理をコメントアウトしてしまったので音が聞こえなくなっていたのを修正

--- Assets/Tamari/Script/SoundManager.cs
+++ Assets/Tamari/Script/SoundManager.cs
@@ -47,7 +47,7 @@ public class SoundManager// : Singleton<SoundManager>
     void ChangeBGM(string cueName)
     {
         //Volumeのフェードアウト
-        DOVirtual.Float(_criAtomBGMSource.volume, 0, _changeSpeed / 2, value => _criAtomBGMSource.volume = value)
+        DOVirtual.Float(_criAtomBGMSource.volume, 0, 1, value => _criAtomBGMSource.volume = value)
             .OnComplete(() =>
             {
                 //設定して再生
