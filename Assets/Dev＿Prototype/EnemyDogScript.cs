7ccbed0a9112d9df53ab131c7e8390c31df16064
ueno-000 <sarasa4869@zoho.com>
2023-01-20 13:23:49 +0900

    [Add] 犬エネミーのSprite、Animの追加

---
+++ Assets/Dev＿Prototype/EnemyDogScript.cs
@@ -0,0 +1,178 @@
+using UnityEngine;
+
+public enum DogType
+{
+    Outrun,//走り抜ける
+    Stop//突然止まる
+}
+
+public class EnemyDogScript : MonoBehaviour
+{
+    [Tooltip("犬の種類"), SerializeField] private DogType _dogType = DogType.Outrun;
+
+    [Tooltip("止まる犬がどこで止まるかの値調整"), SerializeField, Range(0f, 10f)] private float _stopRange = 1;
+
+    /// <summary>エネミーのスピード</summary>
+    private float _speed = 2.0f;
+    public float Speed
+        { get { return _speed; } set { _speed = value; } }
+
+    public EnemyInstansManager EnemyInstansManager { get => _enemyInstansManager; set => _enemyInstansManager = value; }
+
+    private StageMove _stageMove;
+
+
+    /// <summary>エネミーの軸移動方向</summary>
+    private float _startPosX = 0;
+
+    /// <summary>Animation</summary>
+    private Animator _anim = default;
+
+    /// <summary>生成位置のx軸が負の値かの判定</summary>
+    private bool isSpawnNegativeX = false;
+    /// <summary>止まる犬の座る判定</summary>
+    private bool isStop = false;
+    /// <summary>犬生成Manager/// </summary>
+    private EnemyInstansManager _enemyInstansManager;
+    private bool _forwardFast = false;
+
+    private void Start()
+    {
+        _stageMove = GameObject.Find("StageManager").GetComponent<StageMove>();
+        _anim= this.gameObject.transform.GetChild(0).gameObject.GetComponentInChildren<Animator>();
+        Speed = _stageMove._keepSpeed;
+        _startPosX = transform.position.x;
+        isStop = false;
+
+        if (this.gameObject.transform.position.x <= 0)//生成位置が０より小さいのでTrue
+        {
+            isSpawnNegativeX = true;
+
+        }
+        else 
+        {
+            isSpawnNegativeX = false;
+            this.gameObject.transform.GetChild(0).gameObject.GetComponentInChildren<SpriteRenderer>().flipX = true;
+        }
+
+        if (this.gameObject.transform.position.z <= -4 && this.gameObject.transform.position.z >= -7)
+        {
+            this.gameObject.transform.GetChild(0).gameObject.GetComponentInChildren<SpriteRenderer>().sortingOrder= 8;
+        }
+        else if (this.gameObject.transform.position.z <= -6)
+        {
+            this.gameObject.transform.GetChild(0).gameObject.GetComponentInChildren<SpriteRenderer>().sortingOrder = 15;
+        }
+
+        if (_dogType == DogType.Outrun)
+        {
+            SoundManager.Instance.CriAtomPlay(CueSheet.SE, "SE_enemy_small dog_cry");
+        }
+        else 
+        {
+            SoundManager.Instance.CriAtomPlay(CueSheet.SE, "SE_enemy_big dog_cry");
+        }
+
+    }
+
+    private void Update()
+    {
+        if (isStop && isSpawnNegativeX)//座る判定が正になったら且つ進行方向が右(正方向)の犬の場合
+        {
+            Speed = -_stageMove.MoveSpeed;//ステージと同じスピードにする
+        }
+        else if (isStop && !isSpawnNegativeX)//座る判定が正になったら且つ進行方向が左(負方向)の犬の場合
+        {
+            Speed = _stageMove.MoveSpeed;//マイナスをかけステージと同じ方向とスピードにする
+        }
+
+    }
+
+
+    /// <summary>
+    /// エネミーの移動
+    /// </summary>
+    void FixedUpdate()
+    {
+
+        switch (_dogType)
+        {
+            case DogType.Outrun:
+                SoundManager.Instance.CriAtomPlay(CueSheet.SE, "SE._enemy_small dog_breath");
+                //スタート位置によって進行方向とSpriteの向きを変える
+                if (!isSpawnNegativeX)//Playerの進行方向(画面右端から左端に向けて)からくる挙動
+                {
+                        //this.gameObject.transform.GetChild(0).gameObject.GetComponentInChildren<SpriteRenderer>().flipX = true;
+                        this.gameObject.transform.position -= new Vector3(Time.deltaTime * Speed, 0);
+                }
+                else
+                {
+                  this.gameObject.transform.position += new Vector3(Time.deltaTime * Speed, 0);
+                    if (!_forwardFast && this.gameObject.transform.position.x >= 25)
+                    {
+                        EnemyInstansManager.Dog(gameObject);
+                        _forwardFast = true;
+                    }
+                    
+                }
+
+                break;
+
+            case DogType.Stop:
+                SoundManager.Instance.CriAtomPlay(CueSheet.SE, "SE_enemy_big dog_breath");
+                //スタート位置によって進行方向とSpriteの向きを変える
+                if (!isSpawnNegativeX)//Playerの進行方向(画面右端から左端に向けて)からくる挙動
+                {
+                    //this.gameObject.transform.GetChild(0).gameObject.GetComponentInChildren<SpriteRenderer>().flipX = true;
+                    this.gameObject.transform.position -= new Vector3(Time.deltaTime * Speed, 0);
+
+                    //ｘ軸の_stopRngeＭ地点に入ったら止まる(Playerの場所)
+                    if (transform.position.x >= -_stopRange && transform.position.x <= _stopRange)
+                    {
+                        isStop = true;//アニメーションの座る判定を正にする
+                    }
+                    
+                }
+                else//Playerの後ろ方向からくる挙動
+                {
+                    this.gameObject.transform.position += new Vector3(Time.deltaTime * Speed, 0);
+
+                    //ｘ軸の_stopRange地点に入ったら止まる(Playerの場所)
+                    if (transform.position.x >= -_stopRange && transform.position.x <= _stopRange)
+                    {
+                        isStop = true;//アニメーションの座る判定を正にする
+                    }
+                }
+
+
+                break;
+        }
+
+
+        //画面外に行ったら消える
+        if (transform.position.x <= -30 || transform.position.x >= 30)
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (_anim)
+        {
+            _anim.SetBool("isStop",isStop);
+        }
+    }
+
+
+    //あたり判定
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obon")
+        {
+            isStop = true;
+            if(collision.gameObject.TryGetComponent(out Obon obon))
+            {
+                obon.Hit(this.transform.position.x);
+            }
+            //Debug.Log("メイド「あああああああああああ」");
+        }
+    }
+}
