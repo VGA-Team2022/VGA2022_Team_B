af70dde7dbb7cae3930a6e7fd5eaca0f41fb65e2
Akira Watanabe <a0323w@gmail.com>
2023-06-19 06:08:26 +0900

    [update] Resultのパターンを構造体を用いて列挙

--- Assets/Watanabe/Scripts/Result.cs
+++ Assets/Watanabe/Scripts/Result.cs
@@ -1,17 +1,46 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Result : MonoBehaviour
 {
-    [SerializeField] private StoryResult _storyJudge = StoryResult.YashikiStage_Daytime_Clear;
+    [SerializeField] private GameResult _storyJudge = GameResult.YashikiStage_Daytime_Clear;
     [SerializeField] private Sequencer _sequencer = default;
+
+    private Patterns[] _resultPatterns = default;
 
     private void Start()
     {
+        _resultPatterns =
+            new Patterns[]
+            { new Patterns(0, 0, true), new Patterns(0, 0, false),
+              new Patterns(0, 1, true), new Patterns(0, 1, false),
+              new Patterns(1, 0, true), new Patterns(1, 0, false),
+              new Patterns(1, 1, true), new Patterns(1, 1, false) };
+
+        _storyJudge =
+            (GameResult)Array.IndexOf(
+            _resultPatterns,
+            new Patterns(GameManager.GameStageNum, GameManager.StageLevelNum, GameManager.IsGameClear));
+
         _sequencer.SetDialogue(_storyJudge);
+    }
+
+    public struct Patterns
+    {
+        public int Stage;
+        public int Level;
+        public bool IsClear;
+
+        public Patterns(int stage, int level, bool isClear)
+        {
+            Stage = stage;
+            Level = level;
+            IsClear = isClear;
+        }
     }
 }
 
-public enum StoryResult
+public enum GameResult
 {
     YashikiStage_Daytime_Clear,
     YashikiStage_Daytime_Failed,
