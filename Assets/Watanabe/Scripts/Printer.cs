7144353e4e11ac75eb692376badcd577abb03a34
Akira Watanabe <a0323w@gmail.com>
2023-06-19 02:59:08 +0900

    [update] 海ステージのResult対応

---
+++ Assets/Watanabe/Scripts/Printer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary> UIデータの出力を行うクラス </summary>
+[System.Serializable]
+public class Printer
+{
+    [SerializeField] private Fade _fade = default;
+    [Header("Text一覧")]
+    [SerializeField] private Text _speakerText = default;
+    [SerializeField] private Text _dialogueText = default;
+    [Header("Image類のUI")]
+    [SerializeField] private Image _princessImage = default;
+    [SerializeField] private Image _maidImage = default;
+    [SerializeField] private List<Sprite> _princessSprites = new();
+    [SerializeField] private List<Sprite> _maidSprites = new();
+    [Header("Debug")]
+    [Tooltip("Textの表示をどうするか")]
+    [SerializeField] private bool _isTextMove = false;
+
+    /// <summary> 表示するセリフのインデックス </summary>
+    private int _dialogueIndex = 2;
+    /// <summary> セリフ全体 </summary>
+    private string[] _dialogue = default;
+
+    private const string _princess = "お嬢様";
+    private const string _maid = "メイド";
+
+    public void Init(string[] dialogue)
+    {
+        _dialogue = dialogue;
+    }
+
+    /// <summary> セリフ毎にキャラクターのイラストを切り替える </summary>
+    private void SwitchSprite(string speaker, int index)
+    {
+        if (speaker == _princess)
+        {
+            _princessImage.sprite = _princessSprites[index];
+        }
+        else if (speaker == _maid)
+        {
+            _maidImage.sprite = _maidSprites[index];
+        }
+    }
+
+    public void ShowText()
+    {
+        if (_isTextMove)
+        {
+            //1文字ずつ(DOTween使う)
+        }
+        else
+        {
+            //通常表示(ただ切り替えるだけ)
+            if (_dialogueIndex < _dialogue.Length - 1)
+            {
+                var show = _dialogue[_dialogueIndex].Split(',');
+
+                _speakerText.text = show[0];
+                _dialogueText.text = show[1];
+                _dialogueIndex++;
+
+                SwitchSprite(show[0], int.Parse(show[2]));
+            }
+            else
+            {
+                _fade.FadeOut();
+            }
+        }
+    }
+}
---
+++ Assets/Watanabe/Scripts/Sequencer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Sequencer : MonoBehaviour
+{
+    [SerializeField] private TextAsset _textData = default;
+    [SerializeField] private Printer _printer = new();
+
+    private List<string[]> _resultTexts = new();
+
+    private string _loadText = "";
+
+    private void OnEnable()
+    {
+        TextLoad();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _printer.ShowText();
+        }
+    }
+
+    /// <summary> TextAssetのセリフを配列に格納する </summary>
+    private void TextLoad()
+    {
+        _loadText = _textData.text;
+        var messages = _loadText.Split(",,");
+
+        for (int i = 0; i <  messages.Length; i++)
+        {
+            _resultTexts.Add(messages[i].Split("\n"));
+        }
+    }
+
+    public void SetDialogue(StoryResult result)
+    {
+        //以下ResultSceneに表示するTextを設定する処理
+        string[] dialogue = default;
+        switch (result)
+        {
+            //屋敷ステージ
+            case StoryResult.YashikiStage_Daytime_Clear:
+                dialogue = _resultTexts[0];
+                break;
+            case StoryResult.YashikiStage_Night_Clear:
+                dialogue = _resultTexts[1];
+                break;
+            case StoryResult.YashikiStage_Daytime_Failed:
+            case StoryResult.YashikiStage_Night_Failed:
+                dialogue = _resultTexts[2];
+                break;
+            //海ステージ
+            case StoryResult.SeaStage_Daytime_Clear:
+                dialogue = _resultTexts[3];
+                break;
+            case StoryResult.SeaStage_Night_Clear:
+                dialogue = _resultTexts[4];
+                break;
+            case StoryResult.SeaStage_Daytime_Failed:
+            case StoryResult.SeaStage_Night_Failed:
+                dialogue = _resultTexts[5];
+                break;
+        }
+        _printer.Init(dialogue);
+    }
+}
