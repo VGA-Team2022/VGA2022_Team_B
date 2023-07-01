5e6b67dad023f3a7f9efee40b0027483d03348c3
Akira Watanabe <a0323w@gmail.com>
2023-07-02 04:16:32 +0900

    [fix] Resultのボタン表示修正

--- Assets/Ueno/SceneManager/SceneChangeScript.cs
+++ Assets/Ueno/SceneManager/SceneChangeScript.cs
@@ -59,7 +59,6 @@ public class SceneChangeScript
                 //最後までいったらリセット
                 GameManager.GameStageNum = 0;
                 GameManager.StageLevelNum = 0;
-                return false;
             }
         }
         return true;
