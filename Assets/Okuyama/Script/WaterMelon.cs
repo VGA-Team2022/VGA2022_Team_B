700b3b43660c7e799e8addae5c02e479b286af4a
Akira Watanabe <a0323w@gmail.com>
2023-07-06 13:36:37 +0900

    [update] ギミックの挙動を確認中

--- Assets/Okuyama/Script/WaterMelon.cs
+++ Assets/Okuyama/Script/WaterMelon.cs
@@ -4,14 +4,15 @@ public class WaterMelon : GimmickBase
 {
     [Range(0f, 10f)]
     [Tooltip("スイカが止まる範囲")]
-    [SerializeField]
-    private float _stopRange = 1;
+    [SerializeField] private float _stopRange = 1;
     [SerializeField] private Sprite _suikaImage = null;
+    [Tooltip("移動速度の調整値")]
+    [SerializeField] private float _multiplyValue = 2f;
 
     private Animator _animator= null;
     private float _moveSpeed = 1f;
     private bool _isSpawnNegativeX = false;
-    private bool _isStop = false;
+    //private bool _isStop = false;
 
     private void Start()
     {
@@ -20,13 +21,17 @@ public class WaterMelon : GimmickBase
 
         _isSpawnNegativeX = transform.position.x <= 0;
         //移動方向の初期設定
-        if (_isStop)
-        {
-            _moveSpeed = _isSpawnNegativeX ?
+        //if (_isStop)
+        //{
+        //    _moveSpeed = _isSpawnNegativeX ?
+        //        -StageMovement.MoveSpeed : StageMovement.MoveSpeed;
+
+        //    _moveSpeed *= _multiplyValue;
+        //}
+        _moveSpeed = _isSpawnNegativeX ?
                 -StageMovement.MoveSpeed : StageMovement.MoveSpeed;
 
-            _moveSpeed *= 2f;
-        }
+        _moveSpeed *= _multiplyValue;
     }
 
     private void FixedUpdate()
@@ -37,24 +42,28 @@ public class WaterMelon : GimmickBase
     /// <summary> 横移動（CoconutMove.csに準拠） </summary>
     private void Movement()
     {
-        if (!_isSpawnNegativeX)
-        {
+        //if (!_isSpawnNegativeX)
+        //{
+        //    transform.position -= new Vector3(Time.deltaTime * _moveSpeed, 0);
+
+        //    if (transform.position.x >= -_stopRange && transform.position.x <= _stopRange)
+        //    {
+        //        _isStop = true;
+        //    }
+        //}
+        //else
+        //{
+        //    transform.position += new Vector3(Time.deltaTime * _moveSpeed, 0);
+
+        //    if (transform.position.x >= -_stopRange && transform.position.x <= _stopRange)
+        //    {
+        //        _isStop = true;
+        //    }
+        //}
+        //フラグによって移動方向を変える
+        transform.position = _isSpawnNegativeX ?
+            transform.position += new Vector3(Time.deltaTime * _moveSpeed, 0) :
             transform.position -= new Vector3(Time.deltaTime * _moveSpeed, 0);
-
-            if (transform.position.x >= -_stopRange && transform.position.x <= _stopRange)
-            {
-                _isStop = true;
-            }
-        }
-        else
-        {
-            transform.position += new Vector3(Time.deltaTime * _moveSpeed, 0);
-
-            if (transform.position.x >= -_stopRange && transform.position.x <= _stopRange)
-            {
-                _isStop = true;
-            }
-        }
 
         if (transform.position.x <= -30f || transform.position.x >= 30f)
         {
@@ -75,9 +84,8 @@ public class WaterMelon : GimmickBase
             //当たった際の演出を実行し、おぼんを揺らす
             transform.GetChild(0).GetComponentInChildren<SpriteRenderer>().sprite = _suikaImage;
             SoundManager.InstanceSound.PlayAudioClip(SoundManager.SE_Type.Enemy_Coconut);
-            _isStop = true;
+            //_isStop = true;
             obon.Hit(transform.position.x);
-
         }
     }
 }
