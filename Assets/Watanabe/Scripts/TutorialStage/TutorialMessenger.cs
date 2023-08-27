54228d8a70b8d57fb63e4de0b4f2cf5b5d2f0c6c
AkiraWatanabe23 <a0323w@gmail.com>
2023-08-27 14:35:03 +0900

    [update] TutorialのText表示（値割り当て）

--- Assets/Watanabe/Scripts/TutorialStage/TutorialMessenger.cs
+++ Assets/Watanabe/Scripts/TutorialStage/TutorialMessenger.cs
@@ -1,17 +1,62 @@
-﻿using System.Collections.Generic;
+﻿using DG.Tweening;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialMessenger : MonoBehaviour
 {
+    #region UI一覧
+    [Tooltip("セリフのテキストデータ")]
     [SerializeField]
     private TextAsset _textData = default;
 
-    private List<string[]> _showTexts = new();
+    [Header("表示Text一覧")]
+    [Tooltip("何秒かけてセリフを表示させるか")]
+    [SerializeField]
+    private float _indicateTime = 1f;
+    [SerializeField]
+    private Text _speakerNameText = default;
+    [SerializeField]
+    private Text _dialogueText = default;
 
-    private void OnEnable()
+    [Header("Image類のUI")]
+    [SerializeField]
+    private Image _princessImage = default;
+    [SerializeField]
+    private Image _maidImage = default;
+
+    [Header("割り当て用のSprite")]
+    [SerializeField]
+    private Sprite[] _princessSprites = default;
+    [SerializeField]
+    private Sprite[] _maidSprites = default;
+    #endregion
+
+    private List<string[]> _showTexts = new();
+    private bool _isViewText = false;
+
+    private void Start()
     {
         var messages = _textData.text.Split(",,");
-
         for (int i = 0; i < messages.Length; i++) { _showTexts.Add(messages[i].Split("\n")); }
+
+        _speakerNameText.text = "";
+        _dialogueText.text = "";
     }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) { ViewText(); }
+    }
+
+    private void ViewText()
+    {
+        if (_isViewText)
+        {
+            Skip();
+            return;
+        }
+    }
+
+    private void Skip() { _dialogueText.DOComplete(); }
 }
--- Assets/Watanabe/Scripts/TutorialStage/TutorialStage.cs
+++ Assets/Watanabe/Scripts/TutorialStage/TutorialStage.cs
@@ -2,5 +2,8 @@
 
 public class TutorialStage : MonoBehaviour
 {
+    private void Start()
+    {
 
+    }
 }
